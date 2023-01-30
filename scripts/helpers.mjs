a07f8807050b1ad8300778c02bfdeadcda7de951
Themis <idonotwantspams@gmail.com>
2023-01-30 23:34:42 +0000

    Create helpers.mjs
    Copy pasting Zhell's code from Babonus, for getting Distance between source and target Token#5e.

---
+++ scripts/helpers.mjs
@@ -0,0 +1,39 @@
+/** Respect ZHELL's code
+* Gets the minimum distance between two tokens,
+* evaluating all grid spaces they occupy.
+*/
+export function _getMinimumDistanceBetweenTokens(tokenA, tokenB) {
+  const A = _getAllTokenGridSpaces(tokenA.document);
+  const B = _getAllTokenGridSpaces(tokenB.document);
+  const rays = A.flatMap(a => {
+    return B.map(b => {
+      return { ray: new Ray(a, b) };
+    });
+  });
+  const dist = canvas.scene.grid.distance; // 5ft.
+  const distances = canvas.grid.measureDistances(rays, {
+    gridSpaces: false
+  }).map(d => Math.round(d / dist) * dist);
+  const eles = [tokenA, tokenB].map(t => t.document.elevation);
+  const elevationDiff = Math.abs(eles[0] - eles[1]);
+  return Math.max(Math.min(...distances), elevationDiff);
+}
+
+/**
+* Get the upper left corners of all grid spaces a token document occupies.
+*/
+export function _getAllTokenGridSpaces(tokenDoc) {
+  const { width, height, x, y } = tokenDoc;
+  if (width <= 1 && height <= 1) return [{ x, y }];
+  const centers = [];
+  const grid = canvas.grid.size;
+  for (let a = 0; a < width; a++) {
+    for (let b = 0; b < height; b++) {
+      centers.push({
+        x: x + a * grid,
+        y: y + b * grid
+      });
+    }
+  }
+  return centers;
+}
