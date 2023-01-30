7f20181fc060ed5d71a77b0785c25b0db9897c4c
Themis <idonotwantspams@gmail.com>
2023-01-30 23:56:16 +0000

    Create setup.mjs
    Initial commit

---
+++ setup.mjs
@@ -0,0 +1,17 @@
+import {
+  _preRollAbilitySave,
+  _preRollSkill,
+  _preRollAbilityTest,
+  _preRollAttack,
+  _preRollDamage
+} from "./scripts/hooks.mjs";
+
+Hooks.once("init", () => {
+  console.log("Bugbear's Automated Conditions for 5e spinning up!");
+});
+
+Hooks.on("dnd5e.preRollAbilitySave", _preRollAbilitySave);
+Hooks.on("dnd5e.preRollSkill", _preRollSkill);
+Hooks.on("dnd5e.preRollAbilityTest", _preRollAbilityTest);
+Hooks.on("dnd5e.preRollAttack", _preRollAttack);
+Hooks.on("dnd5e.preRollDamage", _preRollDamage);
