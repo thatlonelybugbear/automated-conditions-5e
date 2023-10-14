254748fde1bd4f0e301d1b3eb8be43b0c4291df2
Themis <idonotwantspams@gmail.com>
2023-10-14 03:11:10 +0100

    Update ac5e-helpers.mjs

--- scripts/ac5e-helpers.mjs
+++ scripts/ac5e-helpers.mjs
@@ -1,39 +1,50 @@
-/** Respect ZHELL's code
-* Gets the minimum distance between two tokens,
-* evaluating all grid spaces they occupy.
-*/
+/**
+ * Gets the minimum distance between two tokens,
+ * evaluating all grid spaces they occupy, by Zhell.
+ */
 export function _getMinimumDistanceBetweenTokens(tokenA, tokenB) {
-  const A = _getAllTokenGridSpaces(tokenA.document);
-  const B = _getAllTokenGridSpaces(tokenB.document);
-  const rays = A.flatMap(a => {
-    return B.map(b => {
-      return { ray: new Ray(a, b) };
-    });
-  });
-  const dist = canvas.scene.grid.distance; // 5ft.
-  const distances = canvas.grid.measureDistances(rays, {
-    gridSpaces: true
-  }).map(d => Math.round(d / dist) * dist);
-  const eles = [tokenA, tokenB].map(t => t.document.elevation);
-  const elevationDiff = Math.abs(eles[0] - eles[1]);
-  return Math.max(Math.min(...distances), elevationDiff);
+	const A = _getAllTokenGridSpaces(tokenA.document);
+	const B = _getAllTokenGridSpaces(tokenB.document);
+	const rays = A.flatMap((a) => {
+		return B.map((b) => {
+			return { ray: new Ray(a, b) };
+		});
+	});
+	const dist = canvas.scene.grid.distance; // 5ft.
+	const distances = canvas.grid
+		.measureDistances(rays, {
+			gridSpaces: true,
+		})
+		.map((d) => Math.round(d / dist) * dist);
+	const eles = [tokenA, tokenB].map((t) => t.document.elevation);
+	const elevationDiff = Math.abs(eles[0] - eles[1]);
+	return Math.max(Math.min(...distances), elevationDiff);
 }
 
 /**
-* Get the upper left corners of all grid spaces a token document occupies.
-*/
+ * Get the upper left corners of all grid spaces a token document occupies.
+ */
 export function _getAllTokenGridSpaces(tokenDoc) {
-  const { width, height, x, y } = tokenDoc;
-  if (width <= 1 && height <= 1) return [{ x, y }];
-  const centers = [];
-  const grid = canvas.grid.size;
-  for (let a = 0; a < width; a++) {
-    for (let b = 0; b < height; b++) {
-      centers.push({
-        x: x + a * grid,
-        y: y + b * grid
-      });
-    }
-  }
-  return centers;
+	const { width, height, x, y } = tokenDoc;
+	if (width <= 1 && height <= 1) return [{ x, y }];
+	const centers = [];
+	const grid = canvas.grid.size;
+	for (let a = 0; a < width; a++) {
+		for (let b = 0; b < height; b++) {
+			centers.push({
+				x: x + a * grid,
+				y: y + b * grid,
+			});
+		}
+	}
+	return centers;
+}
+
+export function _getEffects(effectName, effects) {
+	if (typeof effectName === 'string') effectName = [effectName];
+	return effectName.some((name) => effects.some((eff) => eff.name === name));
+}
+
+export function _getConditionName(name) {
+	return eval(`game.i18n.translations.DND5E.Con${name}`);
 }
