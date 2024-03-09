dab2e21f75147d6e989709a2448d979547974225
Themis <idonotwantspams@gmail.com>
2024-03-09 11:07:49 +0000

    Update ac5e-setup.mjs
    revert to v11.11.2

--- scripts/ac5e-setup.mjs
+++ scripts/ac5e-setup.mjs
@@ -5,19 +5,28 @@ import {
 	_preRollAttack,
 	_preRollDamage,
 	_preRollDeathSave,
-	_renderDialog,
-	/*_rollAttack*/
-} from './ac5e-hooks.mjs';
+} from './scripts/ac5e-hooks.mjs';
 
+Hooks.once('init', () => {
+	console.log("Bugbear's Automated Conditions for 5e spinning up!");
+});
+
+Hooks.on('dnd5e.preRollAbilitySave', _preRollAbilitySave);
+Hooks.on('dnd5e.preRollSkill', _preRollSkill);
+Hooks.on('dnd5e.preRollAbilityTest', _preRollAbilityTest);
+Hooks.on('dnd5e.preRollAttack', _preRollAttack);
+Hooks.on('dnd5e.preRollDamage', _preRollDamage);
+Hooks.on('dnd5e.preRollDeathSave', _preRollDeathSave);
+
+//to-do:add renderDialog: ${renderDialog} to v2 also
 Hooks.once('init', () => {
 	const preRollAbilitySave = Hooks.on('dnd5e.preRollAbilitySave',	_preRollAbilitySave);
 	const preRollAbilityTest = Hooks.on('dnd5e.preRollAbilityTest',	_preRollAbilityTest);
 	const preRollAttack = Hooks.on('dnd5e.preRollAttack', _preRollAttack);
-	//const rollAttack = Hooks.on('dnd5e.rollAttack', _rollAttack);
 	const preRollDamage = Hooks.on('dnd5e.preRollDamage', _preRollDamage);
 	const preRollDeathSave = Hooks.on('dnd5e.preRollDeathSave',	_preRollDeathSave);
 	const preRollSkill = Hooks.on('dnd5e.preRollSkill', _preRollSkill);
-	const renderDialog = Hooks.on('renderDialog', _renderDialog);
+	/*const renderDialog = Hooks.on('renderDialog', _renderDialog);*/
 	console.warn(  //to-do: add rollAttack: ${rollAttack} when/if it is enabled
 		`Bugbear's Automated Conditions for 5e added the following dnd5e hooks:
 		preRollAbilitySave: ${preRollAbilitySave}
@@ -26,6 +35,6 @@ Hooks.once('init', () => {
 		preRollDamage: ${preRollDamage}
 		preRollDeathSave: ${preRollDeathSave}
 		preRollSkill: ${preRollSkill}
-		renderDialog: ${renderDialog}`
+		`
 	);
 });
