84d73310c53d198a02a447943b7a10e97e69c3cb
Themis <idonotwantspams@gmail.com>
2024-03-13 13:40:58 +0000

    Update ac5e-settings.mjs

--- scripts/ac5e-settings.mjs
+++ scripts/ac5e-settings.mjs
@@ -3,6 +3,8 @@ import Constants from './ac5e-constants.mjs';
 export default class Settings {
 	// KEYS FOR WORLD CONFIG SETTINGS
 	static SHOW_TOOLTIPS_ROLL_DIALOG = 'showDialogTooltips';
+	static AUTOMATE_ARMOR_PROF_STEALTH = 'autoArmor';
+	static AUTOMATE_RANGED_ATTACKS = 'autoRangedAttacks';
 
 	registerSettings() {
 		this._registerWorldSettings();
@@ -20,11 +22,35 @@ export default class Settings {
 			Constants.MODULE_ID,
 			Settings.SHOW_TOOLTIPS_ROLL_DIALOG,
 			{
-				name: 'Show AC5e tooltips on Roll Dialogs.',
+				name: 'AC5e tooltips on Roll Dialogs.',
 				hint: 'When checked, AC5e will show a helpful tooltip when hovering over the suggested Roll button, summing up the reasons for that suggestion.',
-				scope: 'world',
+				scope: 'client',
 				config: true,
 				default: true,
+				type: Boolean,
+			}
+		);
+		game.settings.register(
+			Constants.MODULE_ID,
+			Settings.AUTOMATE_ARMOR_PROF_STEALTH,
+			{
+				name: 'AC5e armor automation.',
+				hint: 'When checked, AC5e will automatically process proficiency in the equipped piece of armor for ability, skills and save rolls (STR || DEX) and stealth disadvantage property.',
+				scope: 'world',
+				config: true,
+				default: false,
+				type: Boolean,
+			}
+		);
+		game.settings.register(
+			Constants.MODULE_ID,
+			Settings.AUTOMATE_RANGED_ATTACKS,
+			{
+				name: 'AC5e ranged attacks automation.',
+				hint: 'When checked, AC5e will automatically process distance disadvantage/fail based on target distance and range values of item used to attack.',
+				scope: 'world',
+				config: true,
+				default: false,
 				type: Boolean,
 			}
 		);
@@ -33,6 +59,18 @@ export default class Settings {
 		return game.settings.get(
 			Constants.MODULE_ID,
 			Settings.SHOW_TOOLTIPS_ROLL_DIALOG
+		);
+	}
+	get autoArmor() {
+		return game.settings.get(
+			Constants.MODULE_ID,
+			Settings.AUTOMATE_ARMOR_PROF_STEALTH
+		);
+	}
+	get autoRanged() {
+		return game.settings.get(
+			Constants.MODULE_ID,
+			Settings.AUTOMATE_RANGED_ATTACKS
 		);
 	}
 }
