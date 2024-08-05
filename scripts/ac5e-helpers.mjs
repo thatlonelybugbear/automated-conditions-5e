ffb409209d2cbab35bf110bfccee19dedb6b7757
Themis <idonotwantspams@gmail.com>
2024-08-06 02:16:01 +0300

    _getDistance() v12 updates (#151)

--- scripts/ac5e-helpers.mjs
+++ scripts/ac5e-helpers.mjs
@@ -4,45 +4,119 @@ import Settings from './ac5e-settings.mjs';
 const settings = new Settings();
 
 /**
+ * Foundry v12 updated.
  * Gets the minimum distance between two tokens,
- * evaluating all grid spaces they occupy, by Zhell.
+ * evaluating all grid spaces they occupy, by Illandril!.
  */
 export function _getDistance(tokenA, tokenB) {
-	const A = _getAllTokenGridSpaces(tokenA.document);
-	const B = _getAllTokenGridSpaces(tokenB.document);
-	const rays = A.flatMap((a) => {
-		return B.map((b) => {
-			return { ray: new Ray(a, b) };
-		});
-	});
-	const dist = canvas.scene.grid.distance; // 5ft.
-	const distances = canvas.grid
-		.measureDistances(rays, {
-			gridSpaces: true,
-		})
-		.map((d) => Math.round(d / dist) * dist);
-	const eles = [tokenA, tokenB].map((t) => t.document.elevation);
-	const elevationDiff = Math.abs(eles[0] - eles[1]);
-	return Math.max(Math.min(...distances), elevationDiff);
-}
+	if (!tokenA || !tokenB) return false;
+	const PointsAndCenter = {
+		points: [],
+		trueCenternt: {},
+	};
 
-/**
- * Get the upper left corners of all grid spaces a token document occupies.
- */
-export function _getAllTokenGridSpaces(tokenDoc) {
-	const { width, height, x, y } = tokenDoc;
-	if (width <= 1 && height <= 1) return [{ x, y }];
-	const centers = [];
-	const grid = canvas.grid.size;
-	for (let a = 0; a < width; a++) {
-		for (let b = 0; b < height; b++) {
-			centers.push({
-				x: x + a * grid,
-				y: y + b * grid,
-			});
+	const getPolygon = (grid /*: foundry.grid.BaseGrid*/, token /*: Token*/) => {
+		let poly; // PIXI.Polygon;
+		if (token.shape instanceof PIXI.Circle) {
+			poly = token.shape.toPolygon({ density: (token.shape.radius * 8) / grid.size });
+		} else if (token.shape instanceof PIXI.Rectangle) {
+			poly = token.shape.toPolygon();
+		} else {
+			poly = token.shape;
 		}
-	}
-	return centers;
+
+		return new PIXI.Polygon(poly.points.map((point, i) => point + (i % 2 ? token.bounds.top : token.bounds.left)));
+	};
+
+	const getPointsAndCenter = (grid, shape) => {
+		const points = [];
+		for (let i = 0; i < shape.points.length; i += 2) {
+			const x = shape.points[i];
+			const y = shape.points[i + 1];
+			points.push({ x, y });
+
+			const nextX = shape.points[i + 2] ?? shape.points[0];
+			const nextY = shape.points[i + 3] ?? shape.points[1];
+			const d = Math.sqrt((x - nextX) ** 2 + (y - nextY) ** 2);
+			const steps = Math.ceil((d * 2) / grid.size);
+
+			for (let step = 1; step < steps; step++) {
+				points.push({ x: ((nextX - x) / steps) * step + x, y: ((nextY - y) / steps) * step + y });
+			}
+		}
+
+		return {
+			points: points,
+			trueCenter: shape.getBounds().center,
+		};
+	};
+
+	const getPoints = (grid /*: foundry.grid.BaseGrid*/, poly /*: PIXI.Polygon*/) => {
+		const bounds = poly.getBounds();
+		const pointsToMeasure = [bounds.center];
+
+		// If either dimension is one grid space long or less, just use the center point for measurements
+		// Otherwise, we use the center of the grid spaces along the token's perimeter
+		const forcedX = bounds.width <= grid.sizeX ? bounds.center.x : null;
+		const forcedY = bounds.height <= grid.sizeY ? bounds.center.x : null;
+
+		if (typeof forcedX !== 'number' || typeof forcedY !== 'number') {
+			const { points, trueCenter } = getPointsAndCenter(grid, poly);
+			for (const point of points) {
+				const x = (point.x - trueCenter.x) * 0.99 + trueCenter.x;
+				const y = (point.y - trueCenter.y) * 0.99 + trueCenter.y;
+				const pointToMeasure = grid.getCenterPoint({ x, y });
+				pointToMeasure.x = forcedX ?? pointToMeasure.x;
+				pointToMeasure.y = forcedY ?? pointToMeasure.y;
+				if (!pointsToMeasure.some((priorPoint) => priorPoint.x === pointToMeasure.x && priorPoint.y === pointToMeasure.y)) {
+					pointsToMeasure.push(pointToMeasure);
+				}
+			}
+		}
+		return pointsToMeasure;
+	};
+
+	const squareDistance = (pointA /*: Point*/, pointB /*: Point*/) => (pointA.x - pointB.x) ** 2 + (pointA.y - pointB.y) ** 2;
+
+	const getComparisonPoints = (grid /*: foundry.grid.BaseGrid*/, token /*: Token*/, other /*: Token*/) => {
+		const polyA = getPolygon(grid, token);
+		const polyB = getPolygon(grid, other);
+
+		const pointsA = getPoints(grid, polyA);
+		const pointsB = getPoints(grid, polyB);
+		const containedPoint = pointsA.find((point) => polyB.contains(point.x, point.y)) ?? pointsB.find((point) => polyA.contains(point.x, point.y));
+		if (containedPoint) {
+			// A contains B or B contains A... so ensure the distance is 0
+			return [containedPoint, containedPoint];
+		}
+
+		let closestPointA = token.center;
+		let closestPointB = other.center;
+		let closestD2 = squareDistance(closestPointA, closestPointB);
+		for (const pointA of pointsA) {
+			for (const pointB of pointsB) {
+				const d2 = squareDistance(pointA, pointB);
+				if (d2 < closestD2) {
+					closestD2 = d2;
+					closestPointA = pointA;
+					closestPointB = pointB;
+				}
+			}
+		}
+		return [closestPointA, closestPointB];
+	};
+
+	const calculateDistanceWithUnits = (scene /*: Scene*/, grid /*: foundry.grid.BaseGrid*/, token, other /*: Token*/) => {
+		const { distance } = grid.measurePath(getComparisonPoints(grid, token, other));
+
+		return {
+			value: distance,
+			units: scene.grid.units,
+		};
+	};
+	const result = calculateDistanceWithUnits(canvas.scene, canvas.grid, tokenA, tokenB).value;
+	if (settings.debug) console.log(`${Constants.MODULE.NAME.SHORT} - getDistane():`, { sourceId: token.id, targetId: other.id, result });
+	return result;
 }
 
 export function _i18nConditions(name) {
