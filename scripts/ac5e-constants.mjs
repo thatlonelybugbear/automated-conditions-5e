dfc89f14b4303c145dd42fea1fe3849c397c36a4
Themis <idonotwantspams@gmail.com>
2024-08-08 15:33:22 +0100

    Added MODULE_NAME_SHORT

--- scripts/ac5e-constants.mjs
+++ scripts/ac5e-constants.mjs
@@ -2,4 +2,5 @@ export default class Constants {
   static MODULE_ID = 'automated-conditions-5e';
   static MODULE_NAME = 'Automated Conditions 5e';
   static MODULE_ID_SHORT = 'ac5e';
+  static MODULE_NAME_SHORT = 'AC5e';
 }
