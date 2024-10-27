ad6a509dacbe9e8849d2cd4e4644a25c5bcba6b9
Themis <idonotwantspams@gmail.com>
2024-10-27 03:24:28 +0200

    134 feature request enforce ruleswarn for more cases of disallowed spellcasting (#159)
    Implementing #134
    
    - added localization strings for:
      - Silenced and Raging cancelling spell items' use.
      - Rage
      - Raging
      - Subtle Spell
    
    - added checks in preUseItem for:
      - ragingCheck
      - silencedCheck

--- scripts/ac5e-hooks.mjs
+++ scripts/ac5e-hooks.mjs
@@ -577,23 +577,22 @@ export function _renderHijack(renderedType, elem) {
 export function _preUseItem(item, config, options) {
 	const sourceActor = item.actor;
 	if (!sourceActor) return;
-	if (
-		item.type == 'spell' &&
-		settings.autoArmorSpellUse != 'off' &&
-		_autoArmor(sourceActor).notProficient
-	) {
-		if (settings.autoArmorSpellUse == 'warn')
-			ui.notifications.warn(
-				`${sourceActor.name} ${_localize(
-					'AC5E.AutoArmorSpellUseChoicesWarnToast'
-				)}`
-			);
-		else if (settings.autoArmorSpellUse == 'enforce') {
-			ui.notifications.warn(
-				`${sourceActor.name} ${_localize(
-					'AC5E.AutoArmorSpellUseChoicesEnforceToast'
-				)}`
-			);
+	if (item.type == 'spell' && settings.autoArmorSpellUse !== 'off') {
+		if (_autoArmor(sourceActor).notProficient) {
+			if (settings.autoArmorSpellUse === 'warn') ui.notifications.warn(`${sourceActor.name} ${_localize('AC5E.AutoArmorSpellUseChoicesWarnToast')}`);
+			else if (settings.autoArmorSpellUse === 'enforce') {
+				ui.notifications.warn(`${sourceActor.name} ${_localize('AC5E.AutoArmorSpellUseChoicesEnforceToast')}`);
+				return false;
+			}
+		}
+		const ragingCheck = sourceActor.appliedEffects.some((effect) => [_localize('AC5E.Raging'), _localize('AC5E.Rage')].includes(effect.name));
+		const silencedCheck = sourceActor.statuses.has('silenced') && !sourceActor.appliedEffects.some((effect) => effect.name === _localize('AC5E.SubtleSpell'));
+		if (settings.autoArmorSpellUse === 'warn') {
+			if (ragingCheck) ui.notifications.warn(`${sourceActor.name} ${_localize('AC5E.AutoRagingSpellUseChoicesWarnToast')}`);
+			if (silencedCheck) ui.notifications.warn(`${sourceActor.name} ${_localize('AC5E.AutoSilencedSpellUseChoicesWarnToast')}`);
+		} else if (settings.autoArmorSpellUse === 'enforce') {
+			if (ragingCheck) ui.notifications.warn(`${sourceActor.name} ${_localize('AC5E.AutoRagingSpellUseChoicesEnforceToast')}`);
+			if (silencedCheck) ui.notifications.warn(`${sourceActor.name} ${_localize('AC5E.AutoSilencedSpellUseChoicesEnforceToast')}`);
 			return false;
 		}
 	}
