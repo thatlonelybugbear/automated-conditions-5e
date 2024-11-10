65f09eb556c0691e5bf00b6cf362e8ba0863bff4
Themis <idonotwantspams@gmail.com>
2024-11-10 01:07:57 +0000

    Update ac5e-main.mjs

--- scripts/ac5e-main.mjs
+++ scripts/ac5e-main.mjs
@@ -1,16 +1,4 @@
-import {
-	_preRollAbilitySave,
-	_preRollAbilityTest,
-	_preRollAttack,
-	_preRollConcentration,
-	_preRollDamage,
-	_preRollDeathSave,
-	_preRollInitiativeDialog,
-	_preRollSkill,
-	preRollToolCheck,
-	_preUseItem,	
-	_renderHijack,
-} from './ac5e-hooks.mjs';
+import { _preRollConcentration, _preRollAbilitySave, _preRollSkill, _preRollAbilityTest, _preRollAttack, _preRollDamage, _preRollAttackV2, _preRollDamageV2, _preRollDeathSave, _preUseItem, _preUseActivity, _renderHijack } from './ac5e-hooks.mjs';
 import { _systemCheck } from './ac5e-helpers.mjs';
 import Settings from './ac5e-settings.mjs';
 
@@ -23,32 +11,28 @@ Hooks.once('ready', ac5eReady);
 function ac5eRegisterSettings() {
 	if (_systemCheck(4)) rulesVersion = game.settings.get('dnd5e', 'rulesVersion') === 'modern' ? false : true;
 	return new Settings().registerSettings();
-};
+}
 
 function ac5eReady() {
-	if (game.modules.get('midi-qol')?.active) {    
-		Hooks.once('midi-qol.midiReady', ac5eSetup);  //added midi-qol ready hook, so that ac5e registers hooks after MidiQOL.
+	if (game.modules.get('midi-qol')?.active) {
+		Hooks.once('midi-qol.midiReady', ac5eSetup); //added midi-qol ready hook, so that ac5e registers hooks after MidiQOL.
 	} else {
 		ac5eSetup();
 	}
-};
+}
 
 function ac5eSetup() {
 	const v4 = _systemCheck(4);
 	const v4_1 = _systemCheck(4.1);
-	
-	const preRollConcentration = v4_1 
-		? Hooks.on('dnd5e.preRollConcentrationV2', _preRollConcentration)
-		: _systemCheck('3.0.4')
-			? Hooks.on('dnd5e.preRollConcentration', _preRollConcentration)
-			: 'This will only be added for users with dnd5e 3.1+';
+
+	const preRollConcentration = v4_1 ? Hooks.on('dnd5e.preRollConcentrationV2', _preRollConcentration) : _systemCheck('3.0.4') ? Hooks.on('dnd5e.preRollConcentration', _preRollConcentration) : 'This will only be added for users with dnd5e 3.1+';
 	let preRollAttack, preRollDamage, preUseItem;
 	if (v4) {
 		preRollAttack = Hooks.on('dnd5e.preRollAttackV2', _preRollAttack);
 		preRollDamage = Hooks.on('dnd5e.preRollDamageV2', _preRollDamage);
 		preUseItem = Hooks.on('dnd5e.preUseActivity', _preUseItem);
 	} else {
-		preRollAttack =  Hooks.on('dnd5e.preRollAttack', _preRollAttack);
+		preRollAttack = Hooks.on('dnd5e.preRollAttack', _preRollAttack);
 		preRollDamage = Hooks.on('dnd5e.preRollDamage', _preRollDamage);
 		preUseItem = Hooks.on('dnd5e.preUseItem', _preUseItem);
 	}
@@ -71,7 +55,7 @@ function ac5eSetup() {
 	const renderDialog = Hooks.on('renderDialog', _renderHijack);
 	const renderChatMessage = Hooks.on('dnd5e.renderChatMessage', _renderHijack);
 	//to-do: add rollAttack: ${rollAttack} when/if it is enabled
-	console.warn("Automated Conditions 5e added the following (mainly) dnd5e hooks:", {
+	console.warn('Automated Conditions 5e added the following (mainly) dnd5e hooks:', {
 		preRollConcentration,
 		preRollInitiativeDialog,
 		[v4 ? 'preRollAttackV2' : 'preRollAttack']: preRollAttack,
@@ -81,8 +65,9 @@ function ac5eSetup() {
 		[v4_1 ? 'preRollAbilityTestV2' : 'preRollAbilityTest']: preRollAbilityTest,
 		[v4_1 ? 'preRollDeathSaveV2' : 'preRollDeathSave']: preRollDeathSave,
 		[v4_1 ? 'preRollSkillV2' : 'preRollSkill']: preRollSkill,
+		[v4_1 ? 'preRollToolCheckV2' : 'preRollToolCheck']: preRollToolCheck,
 		preRollToolCheckV2,
 		renderDialog,
-		renderChatMessage
+		renderChatMessage,
 	});
 }
