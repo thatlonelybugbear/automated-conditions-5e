88989d2b3ffac5d481b885ddd46eac333efa0a80
Themis <idonotwantspams@gmail.com>
2025-05-17 11:09:43 +0100

    make use of ac5e.showEvaluationData

--- scripts/ac5e-helpers.mjs
+++ scripts/ac5e-helpers.mjs
@@ -997,11 +997,11 @@ export function _createEvaluationSandbox({ subjectToken, opponentToken, options 
 	const statusEffects = CONFIG.statusEffects.map((e) => e.id).concat('bloodied');
 	foundry.utils.mergeObject(sandbox, { CONFIG: { abilities, abilityActivationTypes, activityTypes, attackClassifications, attackModes, attackTypes, creatureTypes, damageTypes, healingTypes, itemProperties, skills, tools, spellSchools, spellcastingTypes, spellLevels, validProperties, weaponTypes, statusEffects } });
 	foundry.utils.mergeObject(sandbox, { checkNearby: ac5e.checkNearby, checkVisibility: ac5e.checkVisibility, checkRanged: ac5e.checkRanged, checkDistance: ac5e.checkDistance, checkCreatureType: ac5e.checkCreatureType, checkArmor: ac5e.checkArmor });
-	if (settings.debug) console.log('AC5E._createEvaluationSandbox:', { sandbox });
 	if (sandbox.undefined) {
 		delete sandbox.undefined; //guard against sandbox.undefined = true being present
 		console.warn('AC5E sandbox.undefined detected!!!');
 	}
+	if (settings.debug || ac5e.showEvaluationData) console.log('AC5E._createEvaluationSandbox:', { evaluationData: sandbox });
 	return sandbox;
 }
 
