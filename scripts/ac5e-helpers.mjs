7297e18ddd335f6cadd338bade6eb8b944e54f2d
Themis <idonotwantspams@gmail.com>
2025-07-24 14:28:37 +0100

    better ammunition sandbox data

--- scripts/ac5e-helpers.mjs
+++ scripts/ac5e-helpers.mjs
@@ -1064,7 +1064,8 @@ export function _createEvaluationSandbox({ subjectToken, opponentToken, options 
 	/* end of backwards compatibility */
 
 	sandbox.activity = activity?.getRollData().activity || {};
-	sandbox.ammunition = options.ammunitionName ? { [options.ammunitionName]: true } : {};
+	sandbox.ammunition = options.ammunition;
+	sandbox.ammunitionName = options.ammunition?.name;
 	sandbox.consumptionItemName = {};
 	sandbox.consumptionItemIdentifier = {};
 	activity?.consumption?.targets?.forEach(({ target }) => {
