56b2e84d8e2f3e0dce1f7908289e244722ac52a5
Themis <idonotwantspams@gmail.com>
2025-10-30 03:05:52 +0000

    Update ac5e-constants.mjs

--- scripts/ac5e-constants.mjs
+++ scripts/ac5e-constants.mjs
@@ -2,4 +2,6 @@ export default class Constants {
   static MODULE_ID = 'automated-conditions-5e';
   static MODULE_NAME = 'Automated Conditions 5e';
   static MODULE_NAME_SHORT = 'ac5e';
+  static GM_EFFECT_DELETIONS = 'gmEffectDeletions';
+  static GM_DOCUMENT_UPDATES = 'gmDocumentUpdates';
 }
