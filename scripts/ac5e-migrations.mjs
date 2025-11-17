7f42a3716d56722836e2da6ab4514b3f3f3ec8a6
Themis <idonotwantspams@gmail.com>
2025-11-17 11:27:49 +0000

    Update ac5e-migrations.mjs - '13.5110.6.3'

--- scripts/ac5e-migrations.mjs
+++ scripts/ac5e-migrations.mjs
@@ -4,25 +4,18 @@ import Settings from './ac5e-settings.mjs';
 const settings = new Settings();
 
 export async function migrate() {
-	const {
-		flags: { lastMigrationPoint },
-		version,
-	} = game.modules.get(Constants.MODULE_ID);
-	if (settings.debug) console.log(lastMigrationPoint);
-	const alreadyMigrated = settings.migrated;
-	if (version >= lastMigrationPoint) {
+	const lastMigrationPoint = '13.5110.6.3';
+	if (settings.migrated === lastMigrationPoint) {
 		console.warn(`${Constants.MODULE_ID} no migration needed`);
 		return null;
 	}
-	//specific execution for 12.422.2
-	if (lastMigrationPoint == '12.422.2') {
-		const oldAutoRanged = settings.autoRanged;
-		const oldAutoNearbyFoes = settings.autoRangedNearbyFoe;
-		if (!oldAutoRanged && !oldAutoNearbyFoes) await game.settings.set(Constants.MODULE_ID, 'autoRangedAttacksMenu', 'off');
-		else if (oldAutoRanged && !oldAutoNearbyFoes) await game.settings.set(Constants.MODULE_ID, 'autoRangedAttacksMenu', 'ranged');
-		else await game.settings.set(Constants.MODULE_ID, 'autoRangedAttacksMenu', 'nearby');
+	if (lastMigrationPoint !== '13.5110.6.3') {
+		const oldRangedSettings = settings.autoRangedCombined;
+		if (!oldRangedSettings) return game.settings.set(Constants.MODULE_ID, 'lastMigratedPoint', lastMigrationPoint);
+		if (oldRangedSettings === 'off') await game.settings.set(Constants.MODULE_ID, 'autoRangeChecks', new Set());
+		else if (oldRangedSettings === 'ranged') await game.settings.set(Constants.MODULE_ID, 'autoRangeChecks', new Set(['meleeOoR', 'rangedOoR', 'rangedLongDisadvantage']));
+		else if (oldRangedSettings === 'nearby') await game.settings.set(Constants.MODULE_ID, 'autoRangeChecks', new Set(['meleeOoR', 'rangedOoR', 'rangedLongDisadvantage', 'rangedNearbyFoes']));
 		await game.settings.set(Constants.MODULE_ID, 'lastMigratedPoint', lastMigrationPoint);
 		console.warn(`${Constants.MODULE_ID} migrated to post ${lastMigrationPoint}`);
-		return true;
 	}
 }
