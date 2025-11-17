170dfaddab83ec8048a7ebf6e172fc6a3f088edd
Themis <idonotwantspams@gmail.com>
2025-11-17 11:58:06 +0000

    Update ac5e-migrations.mjs

--- scripts/ac5e-migrations.mjs
+++ scripts/ac5e-migrations.mjs
@@ -1,21 +1,18 @@
-import Constants from './ac5e-constants.mjs';
-import Settings from './ac5e-settings.mjs';
-
-const settings = new Settings();
-
 export async function _migrate() {
-	const lastMigrationPoint = '13.5110.6.3';
-	if (settings.migrated === lastMigrationPoint) {
+	console.log(settings.migrated)
+	const migration = '13.5110.6.3';
+	const lastMigratedPoint = settings.migrated;
+	if (lastMigratedPoint === migration) {
 		console.warn(`${Constants.MODULE_ID} no migration needed`);
 		return null;
 	}
-	if (lastMigrationPoint !== '13.5110.6.3') {
+	if (lastMigratedPoint !== migration) {
 		const oldRangedSettings = settings.autoRangedCombined;
-		if (!oldRangedSettings) return game.settings.set(Constants.MODULE_ID, 'lastMigratedPoint', lastMigrationPoint);
+		if (!oldRangedSettings) return game.settings.set(Constants.MODULE_ID, 'lastMigratedPoint', migration);
 		if (oldRangedSettings === 'off') await game.settings.set(Constants.MODULE_ID, 'autoRangeChecks', new Set());
 		else if (oldRangedSettings === 'ranged') await game.settings.set(Constants.MODULE_ID, 'autoRangeChecks', new Set(['meleeOoR', 'rangedOoR', 'rangedLongDisadvantage']));
 		else if (oldRangedSettings === 'nearby') await game.settings.set(Constants.MODULE_ID, 'autoRangeChecks', new Set(['meleeOoR', 'rangedOoR', 'rangedLongDisadvantage', 'rangedNearbyFoes']));
-		await game.settings.set(Constants.MODULE_ID, 'lastMigratedPoint', lastMigrationPoint);
-		console.warn(`${Constants.MODULE_ID} migrated to post ${lastMigrationPoint}`);
+		await game.settings.set(Constants.MODULE_ID, 'lastMigratedPoint', migration);
+		console.warn(`${Constants.MODULE_ID} migrated to post ${migration}`);
 	}
 }
