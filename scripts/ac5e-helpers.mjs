b9e859ce6826e5917616c8e170c51675b705d372
Themis <idonotwantspams@gmail.com>
2026-01-21 02:39:39 +0000

    Refactor actionType assignment in sandbox

--- scripts/ac5e-helpers.mjs
+++ scripts/ac5e-helpers.mjs
@@ -1450,7 +1450,7 @@ export function _createEvaluationSandbox({ subjectToken, opponentToken, options 
 	sandbox.spellcastingAbility = activity?.spellcastingAbility;
 	sandbox.messageFlags = activity?.messageFlags;
 	sandbox.activityName = activity ? { [activity.name]: true } : {};
-	sandbox.actionType = activity ? { [activity.actionType]: true } : {};
+	sandbox.actionType = activity ? { [activity.getActionType(options.attackMode)]: true } : {};
 	sandbox.attackMode = options.attackMode ? { [options.attackMode]: true } : {};
 	if (options.attackMode) sandbox._flatConstants[options.attackMode] = true; //backwards compatibility for attack mode directly in the sandbox
 	sandbox.mastery = options.mastery ? { [options.mastery]: true } : {};
@@ -1461,7 +1461,7 @@ export function _createEvaluationSandbox({ subjectToken, opponentToken, options 
 	sandbox.activity.defaultDamageType = options.defaultDamageType;
 	sandbox.activity.attackMode = options.attackMode;
 	sandbox.activity.mastery = options.mastery;
-	if (activity?.actionType) sandbox._flatConstants[activity.actionType] = true;
+	if (activity?.actionType) sandbox._flatConstants[sandbox.actionType] = true;
 	if (activity?.attack?.type) {
 		sandbox._flatConstants[activity.attack.type.value] = true;
 		sandbox._flatConstants[activity.attack.type.classification] = true;
